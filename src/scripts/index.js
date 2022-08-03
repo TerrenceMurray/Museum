c545f4e8f2b17c422bd01a16487db1b7064f478c
Terrence Murray <52611990+TerrenceMurray@users.noreply.github.com>
2022-08-03 11:22:59 -0400

    Structure Complete

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,13 +7,14 @@ import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 let scene, camera, renderer;
 
 init();
+animate();
 
 function init() {
     // Scene
     scene = new THREE.Scene();
 
     // Renderer
-    renderer = new THREE.WEBGLRenderer();
+    renderer = new THREE.WebGLRenderer();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
     renderer.setPixelRatio( window.devicePixelRatio );
@@ -30,10 +31,27 @@ function init() {
         0.1, 
         1000 
     );
+
+    // Event Listener
+    window.addEventListener( 'resize', onWindowResize );
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    controls.handleResize();
+}
+
+function render() {
+    renderer.render( scene, camera );
+
 }
 
 function animate() {
-    requestAnimationFrame(animate);
+    requestAnimationFrame( animate );
 
-    renderer.render( scence, camera );
+    render();
 }
